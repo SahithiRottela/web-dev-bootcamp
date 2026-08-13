1efd0f2fb166f944dec7e63d51f27a9a9f04d478
SahithiRottela <sahithiyadav14907@gmail.com>
2026-08-13 15:03:38 +0530

    temp

--- README.md
+++ README.md
@@ -1,1 +1,1 @@
-# web-dev-bootcamp
+# web-dev-bootcampno
--- server.js
+++ server.js
@@ -3,7 +3,7 @@ const express =require('express');
 const app = express();
 
 const port=process.env.PORT || 8080;
-app.use(express.static("frontend"));
+app.use(express.static("Frontend"));
 app.listen(port,function(){
     console.log("Server running on http://localhost:"+port);
 });
